db88299051649ebed756891775722dd288c2d6ad
kazihabiba201 <kazihabiba125@gmail.com>
2025-11-01 01:08:20 +0600

    -

--- blood_management_system/blood/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import BloodBankViewSet, BloodDonationRequestViewSet
-
-router = DefaultRouter()
-router.register('bloodbanks', BloodBankViewSet)
-router.register('donations', BloodDonationRequestViewSet)
-
-urlpatterns = router.urls
--- blood_management_system/urls.py
+++ blood_management_system/urls.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from accounts.views import register_view, login_view, dashboard_view
+from accounts.views import login_view, register_view, dashboard_view, logout_view
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import logout_view
+
 from accounts.views import admin_dashboard, approve_request, reject_request, logout_view
 # Simple home redirect
 def home_redirect(request):
-    return redirect('dashboard')  
+    if request.user.is_authenticated:
+        return redirect('dashboard')  # Already logged in → go to dashboard
+    else:
+        return redirect('login')
 
 urlpatterns = [
     path('', home_redirect), 
     path('admin/', admin.site.urls),
-    path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
     path('dashboard/', dashboard_view, name='dashboard'),
-    path('api/', include('blood.urls')),
     path('logout/', logout_view, name='logout'),
+    path('api/', include('blood.urls')),
     path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
     path('approve-request/<int:request_id>/', approve_request, name='approve_request'),
     path('reject-request/<int:request_id>/', reject_request, name='reject_request'),
